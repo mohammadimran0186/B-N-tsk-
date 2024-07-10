ffe591aafdc080a4c58d38d5f81b8f9aa563aa5a
mohammadimran0186 <141118387+mohammadimran0186@users.noreply.github.com>
2024-07-10 16:42:08 +0530

    Update index.js

--- node-app/index.js
+++ node-app/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node HOi!!\n'
+  const msg = 'Hello Node test HOi!!\n'
   res.end(msg);
 });
 
